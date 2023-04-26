db315b06a3ef845f17489cac6471862d08702008
Evan Howard <evanhoward@college.harvard.edu>
2023-04-25 20:28:21 -0400

    polish code, add examples, update README

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # CS238
+
+The implementation code for Harvard's CS238 Spring 2023 edition's Final Project. Final Project done by Freddy Reiber, Taj Gulati, and Evan Howard
--- welfare_weights.py
+++ welfare_weights.py
@@ -118,88 +118,107 @@ def get_random_values(total_rent, num_rooms):
     random_numbers = np.diff(random_numbers)
     return list(random_numbers)
 
-# agents_values = [
-#     [1, 8, 1],
-#     [2, 5, 3],
-#     [2, 7, 1]
-# ]
-total_rent = 10
-agents_values = [get_random_values(total_rent, 3) for _ in range(3)]
-# agents_weights = [1, 1, 0.00001]
+def plot_2_agents(agents_values, total_rent, precision):
+    bool_array = np.zeros((precision, precision), dtype=bool)
+    for i in range(1, precision + 1):
+        for j in range(1, precision + 1):
+                bool_array[i - 1, j - 1] = run_instance(agents_values, [i / precision, j / precision], total_rent, False)
+
+    # create a color map that maps True to green and False to red
+    cmap = plt.get_cmap('RdYlGn')
+    cmap.set_bad(color='red')
+    cmap.set_over(color='green')
+    cmap.set_under(color='red')
+
+    # plot the boolean array as an image
+    fig, ax = plt.subplots()
+    ax.imshow(bool_array, cmap=cmap, interpolation='nearest', vmin=0, vmax=1, extent=[1 / precision, 1, 1 / precision, 1])
+    ax.set_xlabel("w_0")
+    ax.set_ylabel("w_1")
+
+    # show the plot
+    plt.show()
+
+def plot_3_agents(agents_values, total_rent, precision):
+    bool_array = np.zeros((precision, precision, precision), dtype=bool)
+    for i in range(1, precision + 1):
+        for j in range(1, precision + 1):
+            for k in range(1, precision + 1):
+                bool_array[i - 1, j - 1, k - 1] = run_instance(agents_values, [i / precision, j / precision, k / precision], total_rent, False)
+
+    # Convert boolean data to integers (1 for True, 0 for False)
+    int_data = bool_array.astype(int)
+
+    # Create a meshgrid for the x, y, and z coordinates
+    x, y, z = np.meshgrid(np.arange(bool_array.shape[0]), np.arange(bool_array.shape[1]), np.arange(bool_array.shape[2]), indexing='ij')
+
+    # Create the 3D plot
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    kw = {
+        'vmin': 0,
+        'vmax': 1,
+        'cmap': plt.cm.get_cmap('RdYlGn', 2),  # Set colormap to RdYlGn and discretize it into 2 levels
+    }
+
+    _ = ax.contourf(
+        x[:, :, -1], y[:, :, -1], int_data[:, :, -1],
+        zdir='z', offset=x.max(), **kw
+    )
+    _ = ax.contourf(
+        x[:, 0, :], int_data[:, 0, :], z[:, 0, :],
+        zdir='y', offset=0, **kw
+    )
+    _ = ax.contourf(
+        int_data[-1, :, :], y[-1, :, :], z[-1, :, :],
+        zdir='x', offset=x.max(), **kw
+    )
+
+    ax.set_xlabel('W_0')
+    ax.set_ylabel('W_1')
+    ax.set_zlabel('W_2')
+
+    xmin, xmax = x.min(), x.max()
+    ymin, ymax = y.min(), y.max()
+    zmin, zmax = z.min(), z.max()
+    ax.set(xlim=[xmin, xmax], ylim=[ymin, ymax], zlim=[zmin, zmax])
+    edges_kw = dict(color='0.8', linewidth=0.5, zorder=1e3)
+    ax.plot([xmax, xmax], [ymin, ymax], [zmax, zmax], **edges_kw)
+    ax.plot([xmin, xmax], [ymin, ymin], [zmax, zmax], **edges_kw)
+    ax.plot([xmax, xmax], [ymin, ymin], [zmin, zmax], **edges_kw)
+
+    ax.set_xticklabels([round(float(item) / precision, 2) for item in ax.get_xticks()])
+    ax.set_yticklabels([round(float(item) / precision, 2) for item in ax.get_yticks()])
+    ax.set_zticklabels([round(float(item) / precision, 2) for item in ax.get_zticks()])
+
+    ax.view_init(30, -60)  # Adjust the viewing angle
+    plt.show()
 
 
-# run_instance(agents_values, agents_weights, total_rent)
+# EXAMPLE
+total_rent = 100
+agents_values_2 = [
+    [80, 20],
+    [50, 50]
+]
+agents_values_3 = [
+    [10, 80, 10],
+    [20, 50, 30],
+    [20, 70, 10]
+]
+agents_values_8 = [
+    [15, 10, 5, 0, 30, 10, 20, 10],
+    [15, 30, 5, 0, 10, 10, 20, 10],
+    [20, 10, 5, 0, 30, 10, 15, 10],
+    [15, 10, 5, 10, 30, 10, 20, 0],
+    [15, 5, 10, 0, 20, 10, 30, 10],
+    [10, 15, 5, 30, 0, 10, 10, 20],
+    [0, 10, 10, 15, 30, 5, 10, 20],
+    [15, 10, 5, 0, 30, 10, 20, 10]
+]
 
-precision = 20
-upper_bound = 1
-bool_array = np.zeros((upper_bound * precision, upper_bound * precision, upper_bound * precision), dtype=bool)
-for i in range(1, (upper_bound * precision) + 1):
-    for j in range(1, (upper_bound * precision) + 1):
-        for k in range(1, (upper_bound * precision) + 1):
-            bool_array[i - 1, j - 1, k - 1] = run_instance(agents_values, [i / precision, j / precision, k / precision], total_rent, False)
+plot_2_agents(agents_values_2, total_rent, 10)
+plot_3_agents(agents_values_3, total_rent, 10)
+run_instance(agents_values_8, [0.5, 0.7, 1, 0.8, 1, 1.2, 1, 0.2], total_rent)
 
-# 2 Agent Visualization
-# create a color map that maps True to green and False to red
-# cmap = plt.get_cmap('RdYlGn')
-# cmap.set_bad(color='red')
-# cmap.set_over(color='green')
-# cmap.set_under(color='red')
-
-# # plot the boolean array as an image
-# fig, ax = plt.subplots()
-# ax.imshow(bool_array, cmap=cmap, interpolation='nearest', vmin=0, vmax=1, extent=[1 / precision, upper_bound, 1 / precision, upper_bound])
-# ax.set_xlabel("w_0")
-# ax.set_ylabel("w_1")
-
-# # show the plot
-# plt.show()
-
-# 3 Agent Visualization
-# Convert boolean data to integers (1 for True, 0 for False)
-int_data = bool_array.astype(int)
-
-# Create a meshgrid for the x, y, and z coordinates
-x, y, z = np.meshgrid(np.arange(bool_array.shape[0]), np.arange(bool_array.shape[1]), np.arange(bool_array.shape[2]), indexing='ij')
-
-# Create the 3D plot
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-
-kw = {
-    'vmin': 0,
-    'vmax': 1,
-    'cmap': plt.cm.get_cmap('RdYlGn', 2),  # Set colormap to RdYlGn and discretize it into 2 levels
-}
-
-_ = ax.contourf(
-    x[:, :, -1], y[:, :, -1], int_data[:, :, -1],
-    zdir='z', offset=x.max(), **kw
-)
-_ = ax.contourf(
-    x[:, 0, :], int_data[:, 0, :], z[:, 0, :],
-    zdir='y', offset=0, **kw
-)
-_ = ax.contourf(
-    int_data[-1, :, :], y[-1, :, :], z[-1, :, :],
-    zdir='x', offset=x.max(), **kw
-)
-
-ax.set_xlabel('W_0')
-ax.set_ylabel('W_1')
-ax.set_zlabel('W_2')
-
-xmin, xmax = x.min(), x.max()
-ymin, ymax = y.min(), y.max()
-zmin, zmax = z.min(), z.max()
-ax.set(xlim=[xmin, xmax], ylim=[ymin, ymax], zlim=[zmin, zmax])
-edges_kw = dict(color='0.8', linewidth=0.5, zorder=1e3)
-ax.plot([xmax, xmax], [ymin, ymax], [zmax, zmax], **edges_kw)
-ax.plot([xmin, xmax], [ymin, ymin], [zmax, zmax], **edges_kw)
-ax.plot([xmax, xmax], [ymin, ymin], [zmin, zmax], **edges_kw)
-
-ax.set_xticklabels([round(float(item) / precision, 2) for item in ax.get_xticks()])
-ax.set_yticklabels([round(float(item) / precision, 2) for item in ax.get_yticks()])
-ax.set_zticklabels([round(float(item) / precision, 2) for item in ax.get_zticks()])
-
-ax.view_init(30, -60)  # Adjust the viewing angle
-plt.show()
